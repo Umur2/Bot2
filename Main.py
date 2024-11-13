87fd3fb993538a951882c7da0abcd987f8dc7c44
Umur2 <fusunaltintas@gmail.com>
2024-11-13 22:04:12 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import discord
+import os
+import random
+import requests
 from sifre import gen_pass
 from discord.ext import commands
 
@@ -23,10 +26,39 @@ async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
+
+
+
+
+@bot.command()
+async def mem(ctx):
+    mems = os.listdir('images')
+    choice = random.choice(mems)
+    with open(f'images/{choice}','rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    
+    
+      
+
+ 
+
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')    
-    
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    '''dog komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run('Discord Token')
